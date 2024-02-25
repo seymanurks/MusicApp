9d8a1261104636e794cb010031c1a38a33eafea7
seymanurks <nurks1905@gmail.com>
2024-02-25 15:48:56 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import {SafeAreaView, View, Text, FlatList, StyleSheet} from "react-native";
 import music_data from "./music-data.json"
 import SongCard from "./components/SongCard";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   const renderSong = ({item}) => <SongCard song = {item}></SongCard>
-
   const renderSeperator = () => <View style = {styles.seperator}/>
+  const [list, setList] = useState(music_data)
+
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase()
+      const songTitle = song.title.toLowerCase()
+
+      return songTitle.indexOf(searchedText) > -1
+    })
+    setList(filteredList)
+  }
 
   return(
     <SafeAreaView style = {styles.container}>
-      <View style = {styles.container}>
+        <SearchBar onSearch={handleSearch}/>
         <FlatList 
-        data = {music_data}
+        data = {list}
         renderItem = {renderSong}
         ItemSeparatorComponent={renderSeperator}
         />
-      </View>
+
     </SafeAreaView>
   )
 }
---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: "#DFDFDF",
+        padding: 8,
+        margin: 5,
+        borderRadius: 5
+    },
+
+})
